ef955214cbb2677f89a0e614dfe587700e6645ce
takerumimata <fossil1908@gmail.com>
2021-05-06 22:15:20 +0900

    define expression statement in ast.go

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -55,3 +55,11 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token // 式の最初のトークン
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
